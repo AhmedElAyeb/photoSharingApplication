98e7f7ef4a84b5c1e92d62dcf7d81c02ad19021a
ahmed elayeb <elayebahmed1@gmail.com>
2018-05-05 12:20:59 +0100

    Modify model classes

--- PhotoSharingApp/Model/Photo.cs
+++ PhotoSharingApp/Model/Photo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +11,13 @@ namespace PhotoSharingApp.Model
     {
         private int PhotoID { get; set; }
         private String Title { get; set; }
-        private List<Byte> PhotoFile { get; set; }
+        [DisplayName("Picture")]  
+        private byte[] PhotoFile { get; set; }
+        [DataType(DataType.MultilineText)]
         private String Description { get; set; }
+        [DisplayName("Created Date")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString ="{0:MM/dd/yy}",ApplyFormatInEditMode =true)]
         private DateTime CreateDate { get; set; }
         private String Owner { get; set; }
         public virtual Photo photo { get; set; }
