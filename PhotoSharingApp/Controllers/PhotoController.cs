cd44eeb9841ed838384c017bddcaf2ad0c4ca71a
ahmed elayeb <elayebahmed1@gmail.com>
2018-05-15 19:31:05 +0100

    creation des methodes create sans parametres dans photocontroller et create avec parametres

--- PhotoSharingApp/Controllers/PhotoController.cs
+++ PhotoSharingApp/Controllers/PhotoController.cs
@@ -14,7 +14,47 @@ namespace PhotoSharingApp.Controller
         public ActionResult Index()
         {
             //var photo = new Photo();
-            return View(context.Photos.First<Photo>());
+            return View(context.Photos.ToList());
+            //context.Photos.First<Photo>()
+        }
+
+        public ActionResult Display(int id)
+        {
+            List<Photo> photos = context.Photos.ToList();
+            var verif = photos.Find(photo => photo.PhotoID == id);
+            if (verif != null)
+                return View("Display",verif);
+            else
+                return HttpNotFound();
+        }
+        public ActionResult Create()
+        {
+            Photo photo = new Photo();
+            photo.CreateDate= DateTime.Now;
+            return View("Create", photo);
+        }
+        [HttpPost]
+        public ActionResult Create(Photo photo, HttpPostedFileBase image)
+        {
+                photo.CreateDate= DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                if (image!=null)
+                {
+                    photo.ImageMimeType = image.ContentType;
+                    photo.PhotoFile = new byte[image.ContentLength];
+                    image.InputStream.Read(photo.PhotoFile,0,image.ContentLength);
+                    context.Photos.Add(photo);
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Create", photo);
+            }
         }
     }
+    
 }
