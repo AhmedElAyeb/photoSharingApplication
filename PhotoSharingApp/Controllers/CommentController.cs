e06c7fcaf8a5df586b129631aa8d25d81f93c28f
ahmed elayeb <elayebahmed1@gmail.com>
2018-06-01 20:43:55 +0100

    creation du dossier Comment sous view, importation du Delete.cshtml et CommentController.cs

---
+++ PhotoSharingApp/Controllers/CommentController.cs
@@ -0,0 +1,49 @@
+﻿using PhotoSharingApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoSharingApp.Controllers
+{
+    public class CommentController : System.Web.Mvc.Controller
+    {
+        private IPhotoSharingContext context;
+
+        //Constructors
+        public CommentController()
+        {
+            context = new PhotoSharingContext();
+        }
+
+        public CommentController(IPhotoSharingContext Context)
+        {
+            context = Context;
+        }
+
+        //
+        // GET: /Comment/Delete/5
+        public ActionResult Delete(int id = 0)
+        {
+            Comment comment = context.FindCommentById(id);
+            ViewBag.PhotoID = comment.PhotoID;
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        //
+        // POST: /Comment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comment comment = context.FindCommentById(id);
+            context.Delete<Comment>(comment);
+            context.SaveChanges();
+            return RedirectToAction("Display", "Photo", new { id = comment.PhotoID });
+        }
+    }
+}
